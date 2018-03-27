5e2a10b2eb10a9424397182257de0f16cb6ad7fc
Tim Vercauteren <tim.vercauteren@kapture.be>
2018-03-27 15:41:41 +0200

    levels

--- BM2/Controllers/LevelsController.cs
+++ BM2/Controllers/LevelsController.cs
@@ -1,13 +1,29 @@
-﻿using BM2.Business.Readers;
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using BM2.Business.Readers;
 using BM2.Business.Writers;
 using BM2.DataAccess.Entities;
+using Microsoft.AspNetCore.Mvc;
 
 namespace BM2.Controllers
 {
+    [Route("[controller]")]
     public class LevelsController : ControllerBase<Level>
     {
         public LevelsController(ILevelReader reader, ILevelWriter writer) : base(reader, writer)
         {
+        }
+
+        [ProducesResponseType(typeof(Level), 200)]
+        public override Task<IActionResult> Get(int id)
+        {
+            return base.Get(id);
+        }
+
+        [ProducesResponseType(typeof(List<Level>), 200)]
+        public override Task<IActionResult> GetAll()
+        {
+            return base.GetAll();
         }
     }
 }
